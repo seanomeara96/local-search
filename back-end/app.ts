30130d291f344a07a5ae527b3ca80f16fcd2f21c
Sean <seanom96@gmail.com>
2020-08-22 21:26:18 +0100

    implemented error handler

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import connect from "./db";
 import router from "./router";
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
 const app = express();
 app.listen(process.env.PORT!, () => {
   console.log("Starting application...");
@@ -9,6 +11,10 @@ app.listen(process.env.PORT!, () => {
       app.use(express.urlencoded({ extended: false }));
       app.use(express.json());
       app.use(router);
+      app.all("*", async () => {
+        throw new NotFoundError();
+      });
+      app.use(errorHandler);
     })
     .catch(() => {
       console.log("Something went wrong...");
